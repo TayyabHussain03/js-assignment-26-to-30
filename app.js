b36e1357d6cdb0b5a8e1c8db00f1f4863a8ed324
TayyabHussain <tayyabfaisal106@gmail.com>
2021-05-27 00:13:04 +0500

    question2 added

--- app.js
+++ app.js
@@ -6,3 +6,13 @@
 // floor = Math.floor(number)
 
 // document.write("<h2>" + "Number: "+ number + "<br/> Round off value: "+ roundNumber + "<br/> Ceil value: "+ ceil +"<br/> Floor value: "+ floor);
+
+
+// **********QUESTION 2**********
+
+// var number = -2.67359
+// roundNumber = Math.round(number)
+// ceil = Math.ceil(number)
+// floor = Math.floor(number)
+
+// document.write("<h2>" + "Number: "+ number + "<br/> Round off value: "+ roundNumber+ "<br/>Ceil value: "+ ceil +"<br/> Floor value: "+ floor);
