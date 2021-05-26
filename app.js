b8a81112f18f7f3a8ee9620b0a76f5ddad95b170
TayyabHussain <tayyabfaisal106@gmail.com>
2021-05-27 00:16:13 +0500

    question3 added

--- app.js
+++ app.js
@@ -16,3 +16,12 @@
 // floor = Math.floor(number)
 
 // document.write("<h2>" + "Number: "+ number + "<br/> Round off value: "+ roundNumber+ "<br/>Ceil value: "+ ceil +"<br/> Floor value: "+ floor);
+
+
+// **********QUESTION 3**********
+
+// userValue = +prompt("Enter any number ")
+
+// number = Math.abs(userValue)
+
+// document.write("<h2>" + "The user value is: "+ userValue +" <br/>The absolute value is: "+ number);
